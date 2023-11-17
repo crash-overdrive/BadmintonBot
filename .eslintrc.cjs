88444e008ab9885543240d59f987555352858f18
Shashwat Pratap <pratapshashwat@gmail.com>
2023-11-17 01:33:29 -0500

    Add linter

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        'eslint:recommended', 
+        'plugin:@typescript-eslint/recommended-type-checked',
+    ],
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+    },
+    root: true,
+    ignorePatterns: ["built/*"],
+};
